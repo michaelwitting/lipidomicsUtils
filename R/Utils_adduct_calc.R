c5038fd760321407af806fd3a4051058534a5c4b
Michael Witting <michael.witting@helmholtz-muenchen.de>
2019-08-18 23:00:38 +0200

    reuse functions form metabolomicsUtils

--- R/Utils_adduct_calc.R
+++ R/Utils_adduct_calc.R
@@ -16,16 +16,7 @@
 #' @export
 calc_adduct_mass <- function(exact_mass, adduct) {
   
-  adduct_rule_list <- adduct_rules()
-  
-  if(!adduct %in% names(adduct_rule_list)) {
-    stop(paste0("Unknown adduct: ", adduct))
-  }
-  
-  mass_multi <- adduct_rule_list[[adduct]]$mass_multi
-  mass_add <- adduct_rule_list[[adduct]]$mass_add
-  
-  ion_mass <- exact_mass * mass_multi + mass_add
+  ion_mass <- metabolomicsUtils::calc_adduct_mass(exact_mass, adduct)
   
   return(ion_mass)
   
@@ -49,16 +40,7 @@ calc_adduct_mass <- function(exact_mass, adduct) {
 #' @export
 calc_neutral_mass <- function(ion_mass, adduct) {
   
-  adduct_rule_list <- adduct_rules()
-  
-  if(!adduct %in% names(adduct_rule_list)) {
-    stop(paste0("Unknown adduct: ", adduct))
-  }
-  
-  mass_multi <- adduct_rule_list[[adduct]]$mass_multi
-  mass_add <- adduct_rule_list[[adduct]]$mass_add
-  
-  exact_mass <- (ion_mass - mass_add) / mass_multi
+  exact_mass <- metabolomicsUtils::calc_neutral_mass(ion_mass, adduct)
   
   return(exact_mass)
   
@@ -81,24 +63,8 @@ calc_neutral_mass <- function(ion_mass, adduct) {
 #'
 #' @export
 create_ion_formula <- function(chem_formula, adduct) {
-  
-  # get all adduct rules
-  adduct_rules_all <- adduct_rules()
-  
-  # create ion formula
-  ion_formula <- formula_addition(chem_formula, adduct_rules_all[[adduct]]$formula_add)
-  ion_formula <- formula_subtraction(ion_formula, adduct_rules_all[[adduct]]$formula_sub)
-  
-  # add brackets and charge
-  if(stringr::str_detect(adduct, "\\]\\+")) {
-    
-    ion_formula <- paste0("[", ion_formula, "]", abs(adduct_rules_all[[adduct]]$charge), "+")
-    
-  } else if(stringr::str_detect(adduct, "\\]\\-")) {
-    
-    ion_formula <- paste0("[", ion_formula, "]", abs(adduct_rules_all[[adduct]]$charge), "-")
-    
-  }
+
+  ion_formula <- metabolomicsUtils::create_ion_formula(chem_formula, adduct)
 
   # return result
   return(ion_formula)
